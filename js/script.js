f85686c67e73baca240114b866f0050c6a4ac95f
Gabriel Antonio Machado da Silva <gabriel.machado.a@gmail.com>
2021-07-16 01:42:53 -0300

    Arquivos iniciais adicionados

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function store(){
+     var inputEmail= document.getElementById("email");
+     localStorage.setItem("email", inputEmail.value);
+    }
+
+function store(){
+      var inputEmail= document.getElementById("email-products");
+      localStorage.setItem("email-products", inputEmail.value);
+     }
+
+function store(){
+      var inputEmail= document.getElementById("email-footer");
+      localStorage.setItem("email-footer", inputEmail.value);
+      var inputMessage= document.getElementById("mensagem");
+      localStorage.setItem("mensagem", inputMessage.value);
+     }
